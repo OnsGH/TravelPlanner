f35c8bdd313ba3ab7899a8c54534ca6d50f863df
Ons Ghariani <ons@Onss-MacBook-Pro.local>
2021-02-14 20:48:34 -0800

    third  commit,formatter works on files using webformatter

--- src/client/index.js
+++ src/client/index.js
@@ -1,21 +1,12 @@
-import { displaySentimentAnalysisInfo } from './js/displaySentimentAnalysisData'
-import{ isEmptyInput } from './js/displaySentimentAnalysisData'
-import { handleSubmit } from './js/formHandler'
+import { displaySentimentAnalysisInfo } from "./js/displaySentimentAnalysisData";
+import{ getRowsCount } from "./js/displaySentimentAnalysisData";
+import { isEmptyInput } from "./js/displaySentimentAnalysisData";
+import { handleSubmit } from "./js/formHandler";
 
-import './styles/resets.scss'
-import './styles/base.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
-import './styles/header.scss'
+import "./styles/resets.scss";
+import "./styles/base.scss";
+import "./styles/footer.scss";
+import "./styles/form.scss";
+import "./styles/header.scss";
 
-//console.log(checkForName);
-
-//alert("I EXIST")
-
-export {
-    displaySentimentAnalysisInfo,
-    isEmptyInput,
-    handleSubmit
-}
-
-
+export { getRowsCount, displaySentimentAnalysisInfo, isEmptyInput, handleSubmit };
--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,16 @@
-
-
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-var path = require('path')
-const fetch = require('node-fetch');
-const mockAPIResponse = require('./mockAPI.js')
+var path = require("path");
+const fetch = require("node-fetch");
+const mockAPIResponse = require("./mockAPI.js");
 const express = require("express");
 
 // Start up an instance of app
-const app = express()
+const app = express();
 
-
-const apiKey = process.env.API_KEY ;
-//${process.env.API_KEY}
-let projectData = []
+const apiKey = process.env.API_KEY;
+let projectData = [];
 
 /* Dependencies */
 const bodyParser = require("body-parser");
@@ -26,37 +22,30 @@ app.use(bodyParser.json());
 const cors = require("cors");
 app.use(cors());
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-console.log(__dirname)
+console.log(__dirname);
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-   res.sendFile(path.resolve('src/client/views/index.html'))
-})
+app.get("/", function (req, res) {
+    res.sendFile(path.resolve("src/client/views/index.html"));
+});
 
-
-
-
-app.post('/sentimentAnalysisApi', async function(req, res) {
+app.post("/sentimentAnalysisApi", async function (req, res) {
     projectData = req.body.textToAnalyse;
     console.log(projectData);
-    const ApiUrl = `https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&lang=en&txt=${projectData}`
+    const ApiUrl = `https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&lang=en&txt=${projectData}`;
 
-    const response = await fetch(ApiUrl)
-    const sentimentAnalysisData = await response.json()
-    console.log(sentimentAnalysisData)
-    res.send(sentimentAnalysisData)
-})
-
-
+    const response = await fetch(ApiUrl);
+    const sentimentAnalysisData = await response.json();
+    console.log(sentimentAnalysisData);
+    res.send(sentimentAnalysisData);
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log(`Your API key issss ${apiKey}`);
-    console.log('Example app listening on port 8081!')
-})
+    console.log("Example app listening on port 8081!");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+    res.send(mockAPIResponse);
+});
